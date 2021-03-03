73af6ca15c517f813d75bebcf5e95e676a1b2efe
AmoDinho <u14284783@tuks.co.za>
2021-03-03 21:27:50 +0200

    GETTING EVERYTHING setup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setPageHeader } from "./slices/layoutSlice";
 import Routes from "./routes";
-const App = () => <Routes />;
+const App = () => {
+  const title = useSelector((state) => state.layout.pageHeader);
+  const dispatch = useDispatch();
+  useEffect(() => {}, []);
+  return (
+    <div>
+      <Routes />
+    </div>
+  );
+};
 export default App;
