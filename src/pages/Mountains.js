e17a4730ef9277f4ca5ab17b41a445c6a07cb791
AmoDinho <u14284783@tuks.co.za>
2021-03-03 21:42:06 +0200

    created mounains file

---
+++ src/pages/Mountains.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addMountain } from "../slices/mountainsSlice";
+
+const Mountains = (props) => {
+  return <div></div>;
+};
+
+export default Mountains;
