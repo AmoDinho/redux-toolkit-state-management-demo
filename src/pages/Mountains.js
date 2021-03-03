46e99d0cb4084d3a81f101b4e5f8bb1cd1a898f5
AmoDinho <u14284783@tuks.co.za>
2021-03-03 21:45:02 +0200

    added stuff

--- src/pages/Mountains.js
+++ src/pages/Mountains.js
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addMountain } from "../slices/mountainsSlice";
+import { setPageHeader } from "../slices/layoutSlice";
 
 const Mountains = (props) => {
   const mountains = useSelector((state) => state.mountains.mountains);
   const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(setPageHeader("Mountains"));
+  }, []);
+
   return <div></div>;
 };
 
