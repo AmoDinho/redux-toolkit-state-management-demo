2c3547184228a18026000fb9cf1d05d317c03d35
AmoDinho <u14284783@tuks.co.za>
2021-03-03 21:35:05 +0200

    created mountain slice

---
+++ src/slices/mountainsSlice.js
@@ -0,0 +1,8 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const mountainsSlice = createSlice({
+  name: "mountains",
+  initialState: {
+    mountains: ["Kasteelspoort", "Jonkershoek", "Mont Rochelle"],
+  },
+});
