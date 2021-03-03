75e66e383709f055ea6f8d7502237fb679cc928e
AmoDinho <u14284783@tuks.co.za>
2021-03-03 21:39:54 +0200

    added the mountain slice

--- src/slices/mountainsSlice.js
+++ src/slices/mountainsSlice.js
@@ -1,9 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
-
+import { addNewMountain } from "../reducers/mountainsRedcuers";
 export const mountainsSlice = createSlice({
   name: "mountains",
   initialState: {
     mountains: ["Kasteelspoort", "Jonkershoek", "Mont Rochelle"],
   },
-  reducers: {},
+  reducers: {
+    addMountain: (state, action) => addNewMountain(state, action),
+  },
 });
+
+export const { addMountain } = mountainsSlice.actions;
+export default mountainsSlice.reducer;
