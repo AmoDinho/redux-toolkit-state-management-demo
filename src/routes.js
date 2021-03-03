112ad6833062c4256214101fb65d6ee76042a7e7
AmoDinho <u14284783@tuks.co.za>
2021-03-03 21:42:47 +0200

    exported the route

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Router } from "@reach/router";
 import Home from "./pages/index";
+import Mountains from "./pages/Mountains";
 const Routes = ({ props }) => {
   return (
     <Router>
       <Home path="/" props={props} />
+      <Mountains path="/mountains" props={props} />
     </Router>
   );
 };
