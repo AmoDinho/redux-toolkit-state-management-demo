d0410cfd0332ca3178970bbe139b9bc0977f1701
AmoDinho <u14284783@tuks.co.za>
2021-06-06 20:24:15 +0200

    Routing worked

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { setPageHeader } from "../slices/layoutSlice";
 import HeadingOne from "../components/HeadingOne";
+import { Link } from "@reach/router";
 
 const Index = () => {
   const dispatch = useDispatch();
@@ -16,6 +17,10 @@ const Index = () => {
         This is the root of the app. Above is the header that is controlled by
         Redux .
       </HeadingOne>
+
+      <Link to="/mountains">
+        Lets go and learn about the Cape's finest mountains!
+      </Link>
     </>
   );
 };
